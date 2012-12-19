6872c11a2ac6f659d61c7f1c451336e0afbaf304
huangqimin <huangqimin@baidu.com>
2012-12-19 17:34:46 +0800

    alter function FetchPicFromBing, delete temp file code, direct save as bmp format

--- SetWallpaperFromBing.py
+++ SetWallpaperFromBing.py
@@ -3,28 +3,20 @@
 
 import requests
 import time
-import Image
+from PIL import Image
+from StringIO import StringIO
 import os
-import win32gui,win32con,win32api
 
-def FetchPicFormBing(ImagePath):
+import win32gui,win32con,win32api
+
+def FetchPicFromBing(ImagePath):
     # 获取Bing首页图片的真实地址
     r = requests.get('http://cn.bing.com/')
     r = requests.get(r.text.split("g_img={url:'")[1].split("'")[0])
 
-    # 输入图片的类型(gep、bmp、gif)
-    #print r.headers['content-type']
-
-    # 保存图片到本地(直接以jpg为后缀，这个对图片格式没有影响)
-    f = open("C:\\temp.jpg", "wb")
-    f.write(r.content)
-    f.close()
-
-    # 将图片变为bmp格式
-    img = Image.open("C:\\temp.jpg")
+    # 将图片保存为bmp格式
+    img = Image.open(StringIO(r.content))
     img.save(ImagePath)
-    os.remove("C:\\temp.jpg")
-
 
 def SetWallpaperFromBMP(ImagePath):
     k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
@@ -34,5 +26,5 @@ def SetWallpaperFromBMP(ImagePath):
 
 if __name__ == "__main__":
     ImagePath = "C:\\"+time.strftime('%Y-%m-%d',time.localtime(time.time()))+".bmp"
-    FetchPicFormBing(ImagePath)
+    FetchPicFromBing(ImagePath)
     SetWallpaperFromBMP(ImagePath)
